8e61d1f8d7dedff5f975c7fc76f680fe4ade7e3e
Aaron Wertman <aaron.wertman@gmail.com>
2014-06-05 17:43:53 -0700

    add posts controller to manage ajax

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,9 +2,8 @@ class BrandsController < ApplicationController
 
   def show
     # if user signed in, else show id 1
-    # @brand = Brand.find_by(id: params[:id])
     @brand_id = params[:id]
-    @tag_options = Brand.get_filtered_tags_by( @brand_id, 20 ) # currently not using limit
+    @tag_options = Brand.get_filtered_tags_by( @brand_id, 20 )
     @posts = Post.get_and_sort_by_tag_matches( @brand_id, @tag_options )
 
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   def show
     p "---------- inside posts controller -----------"
     p params
+    @tag_options = Brand.get_tags_by_filters()
     return {test: "info"}.to_json
   end
 
